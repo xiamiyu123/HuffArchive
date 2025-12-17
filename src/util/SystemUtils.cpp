e34bdf39948724ea8d976eee6ad3cf3aa466981c
xiamiyu123 <128821702+xiamiyu123@users.noreply.github.com>
2025-12-17 23:13:02 +0800

    fix:现在使用系统api修复

--- src/util/SystemUtils.cpp
+++ src/util/SystemUtils.cpp
@@ -11,33 +11,48 @@
 
 namespace Util {
 
+#ifdef _WIN32
+bool setRegistryValue(HKEY hKeyRoot, const QString& subKey, const QString& valueName, const QString& data) {
+    HKEY hKey;
+    std::wstring wSubKey = subKey.toStdWString();
+    LONG result = RegCreateKeyExW(hKeyRoot, wSubKey.c_str(), 0, NULL, 
+                                  REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL);
+    if (result != ERROR_SUCCESS) {
+        return false;
+    }
+
+    std::wstring wValueName = valueName.toStdWString();
+    std::wstring wData = data.toStdWString();
+    result = RegSetValueExW(hKey, valueName.isEmpty() ? NULL : wValueName.c_str(), 
+                            0, REG_SZ, (const BYTE*)wData.c_str(), 
+                            (wData.length() + 1) * sizeof(wchar_t));
+    
+    RegCloseKey(hKey);
+    return result == ERROR_SUCCESS;
+}
+#endif
+
 bool SystemUtils::registerFileAssociation() {
 #ifdef _WIN32
     QString appPath = QDir::toNativeSeparators(QCoreApplication::applicationFilePath());
     QString className = "HuffmanArchive";
     QString ext = ".huff";
+    QString description = "Huffman Archive";
     
-    // HKEY_CURRENT_USER\Software\Classes
-    QSettings classes(QSettings::UserScope, "Microsoft", "Windows\\CurrentVersion\\Explorer\\FileExts");
-    // Note: QSettings with NativeFormat on Windows usually maps to HKCU\Software\Organization\App
-    // To write to HKCU\Software\Classes directly, we use QSettings with specific path
+    // 1. HKCU\Software\Classes\.huff -> Default = HuffmanArchive
+    if (!setRegistryValue(HKEY_CURRENT_USER, "Software\\Classes\\" + ext, "", className)) return false;
     
-    // Using QSettings to write to registry directly
-    QSettings settings("HKEY_CURRENT_USER\\Software\\Classes", QSettings::NativeFormat);
+    // 2. HKCU\Software\Classes\HuffmanArchive -> Default = Huffman Archive
+    if (!setRegistryValue(HKEY_CURRENT_USER, "Software\\Classes\\" + className, "", description)) return false;
     
-    // 1. Associate .huff with HuffmanArchive class
-    settings.setValue(ext + "/.", className);
+    // 3. HKCU\Software\Classes\HuffmanArchive\DefaultIcon -> Default = "appPath",0
+    QString iconPath = "\"" + appPath + "\",0";
+    if (!setRegistryValue(HKEY_CURRENT_USER, "Software\\Classes\\" + className + "\\DefaultIcon", "", iconPath)) return false;
     
-    // 2. Define HuffmanArchive class
-    settings.setValue(className + "/.", "Huffman Archive");
+    // 4. HKCU\Software\Classes\HuffmanArchive\shell\open\command -> Default = "appPath" "%1"
+    QString command = "\"" + appPath + "\" \"%1\"";
+    if (!setRegistryValue(HKEY_CURRENT_USER, "Software\\Classes\\" + className + "\\shell\\open\\command", "", command)) return false;
     
-    // 3. Set Default Icon
-    settings.setValue(className + "/DefaultIcon/.", QString("\"" + appPath + "\",0"));
-    
-    // 4. Set Open Command
-    settings.setValue(className + "/shell/open/command/.", QString("\"" + appPath + "\" \"%1\""));
-    
-    settings.sync();
     SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, NULL, NULL);
 
     return true;
@@ -49,10 +64,24 @@ bool SystemUtils::registerFileAssociation() {
 
 bool SystemUtils::isFileAssociationRegistered() {
 #ifdef _WIN32
-    QSettings settings("HKEY_CURRENT_USER\\Software\\Classes", QSettings::NativeFormat);
-    QString cmd = settings.value("HuffmanArchive/shell/open/command/.").toString();
+    QString className = "HuffmanArchive";
+    QString subKey = "Software\\Classes\\" + className + "\\shell\\open\\command";
+    
+    HKEY hKey;
+    LONG result = RegOpenKeyExW(HKEY_CURRENT_USER, subKey.toStdWString().c_str(), 0, KEY_READ, &hKey);
+    if (result != ERROR_SUCCESS) return false;
+    
+    wchar_t buffer[MAX_PATH * 2];
+    DWORD bufferSize = sizeof(buffer);
+    result = RegQueryValueExW(hKey, NULL, NULL, NULL, (LPBYTE)buffer, &bufferSize);
+    RegCloseKey(hKey);
+    
+    if (result != ERROR_SUCCESS) return false;
+    
+    QString command = QString::fromWCharArray(buffer);
     QString appPath = QDir::toNativeSeparators(QCoreApplication::applicationFilePath());
-    return cmd.contains(appPath);
+    
+    return command.contains(appPath);
 #else
     return false;
 #endif
